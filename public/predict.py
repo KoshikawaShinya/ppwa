d3752133953ec496ffdc22e8d0cbe881421c97dc
sin528 <satowimp0319@gmail.com>
2020-08-17 20:06:53 +0900

    2020/08/17:1

--- public/predict.py
+++ public/predict.py
@@ -6,7 +6,16 @@ import pickle
 import numpy as np
 import tensorflow as tf
 
-# 整形しろ
+
+def cut(img_path):
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    x, y = img.shape[1] // 2, img.shape[0] // 2,
+    new_img = img[max([0, y - w]) : y + w, max([0, x - w]) : x + w]
+    resized_img = cv2.resize(new_img, (2*w, 2*w))
+    reshaped_img = img.reshape([1, img_size, img_size, -1])
+    return reshaped_img
+
+
 img_size = 500
 
 with open('count.pickle', 'rb') as f:
@@ -15,9 +24,9 @@ with open('count.pickle', 'rb') as f:
 model = tf.keras.models.load_model('saved_model/PredictFruit_' + str(count) + '.h5')
 for img_path in glob.glob('../storage/app/public/judge/origin/*'):
 
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    reshaped_img = img.reshape([1, img_size, img_size, -1])
+    img = cut(img_path)
     predict = model.predict(reshaped_img)
     num = np.argmax(predict) + 1
 
     print(num)
+    os.remove(img_path)
